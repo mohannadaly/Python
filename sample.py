386ea26879cb018b66ab990ae8296f2daad11e1e
Mohannad Ayman <mohannadaly98@gmail.com>
2018-08-29 06:54:45 +0200

    Testing Class interaction.

--- sample.py
+++ sample.py
@@ -1,11 +1,28 @@
 class Person:
-    def __init__(self, name, origin, age):
+    def __init__(self, name, origin, age, isSitting, ownedRobot):
         self.name = name
         self.origin = origin
         self.age = age
+        self.isSitting = isSitting
+        self.ownedRobot = ownedRobot
     def introduceSelf(self):
         print('My name is', self.name)
         print('I\'m from', self.origin)
         print('I\'m', self.age, 'years old')
-r = Person('mKay', 'Egypt', 20)
+    def sit_down(self):
+        self.isSitting = True
+    def stand_up(self):
+        self.isSitting = False
+class Robot:
+    def __init__(self, name, color, age):
+        self.name = name
+        self.color = color
+        self.age = age
+    def introduceSelf(self):
+        print('My name is', self.name)
+
+r = Robot('Krusanity', 'Black', 69)
+p = Person('mKay', 'Egypt', 20, False, 'r')
 r.introduceSelf()
+a = p.ownedRobot
+a.introduceSelf()
