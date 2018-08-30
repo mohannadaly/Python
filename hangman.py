ada675415ee4ad4800eb4f27d57d6c78e11546c3
mohannadaly <mohannadaly98@gmail.com>
2018-08-30 12:16:07 +0200

    Implement hint function.

--- hangman.py
+++ hangman.py
@@ -5,21 +5,24 @@ words = open('hangman_words.txt', 'r').read().splitlines()
 descriptions = open('hangman_descriptions.txt', 'r').read().splitlines()
 chosen_index = choose_from(range(len(words)))
 chosen_word = words[chosen_index]
+chosen_description = descriptions[chosen_index]
 letter_check = chosen_word.lower()
 letter_check_len = len(letter_check)
-chosen_description = descriptions[chosen_index]
 attempts = 8
 user_won = False
+# core code of the game
 print('Available attempts:', attempts)
 while True:
     if letter_check_len == 0:
         user_won = True
         break
     user_attempt = input('What is the word?\n').lower().replace(' ', '')
-    if user_attempt.lower() in ['quit', 'exit', 'done']:
+    if user_attempt.lower() in ['quit', 'exit', 'done']: # quit check
         print('Quitting...')
         sleep(1)
         quit()
+    elif user_attempt.lower() in ['hint', 'help']:
+        print(chosen_description)
     elif user_attempt == '':
         print('Invalid attempt, try again.')
     elif len(user_attempt) < len(chosen_word): # check the letters
@@ -39,6 +42,7 @@ while True:
             else:
                 user_won = False
                 break
+# outcome of the game
 if user_won:
     print('You Win!!! Congratulations.')
 else:
